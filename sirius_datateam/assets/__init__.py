1db9233d81bd240dc6c5869fbda45d7ef91c71f3
pete <thirapongpdg@gmail.com>
2022-10-04 11:53:19 +0700

    fix cereal.py

--- sirius_datateam/assets/__init__.py
+++ sirius_datateam/assets/__init__.py
@@ -6,7 +6,7 @@ from dagster import asset, get_dagster_logger, load_assets_from_package_module
 
 from sirius_datateam.assets import cereal
 
-CEREAL = "cereal"
-cereal_assets = load_assets_from_package_module(
-    package_module=cereal, group_name=CEREAL
-)
+# CEREAL = "cereal"
+# cereal_assets = load_assets_from_package_module(
+#     package_module=cereal, group_name=CEREAL
+# )
