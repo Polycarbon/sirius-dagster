0b0199e3fd9a80fb16eacc7662415424cc344e72
pete <thirapongpdg@gmail.com>
2022-10-04 13:05:29 +0700

    fix group name

--- sirius_datateam/jobs.py
+++ sirius_datateam/jobs.py
@@ -1,8 +1,8 @@
 from dagster import job
 
-from sirius_datateam.ops.hwc_api import get_all_resources, get_token
 
-
-@job
-def hwc_resource_ingest():
-    get_all_resources(get_token())
+#
+#
+# @job
+# def hwc_resource_ingest():
+#     get_all_resources(get_token())
